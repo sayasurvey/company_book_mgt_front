e14ff18809ac93d6f63d39e05d29d8d7bdbde77d
ユーザ名 <sayasurvey@yahoo.co.jp>
2023-09-18 18:10:23 +0900

    FIX: 本のデータの修正
    FIX: １列の本を４つから５つに変更
    
    ADD: 画像の下にボタンを表示
    
    ADD: 貸出中の場合は借りるをグレーアウト
    
    FIX: デザインの微調整

--- src/components/booklist.tsx
+++ src/components/booklist.tsx
@@ -3,26 +3,31 @@ import { DefaultButton }  from './default_button';
 
 interface Book {
   imageUrl: string;
-  altText: string;
   title: string;
+  author: string;
+  loanable: boolean;
 }
 
 interface BookCardProps extends Book {}
 
-function BookCard({ imageUrl, altText, title }: BookCardProps): ReactElement {
+function BookCard({ imageUrl, title, author, loanable }: BookCardProps): ReactElement {
   return (
     <div className='border rounded-lg'>
       <a href="#" className="rounded-lg">
-        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-tlg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        <div className="group relative aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
           <img
             src={imageUrl}
-            alt={altText}
-            className="h-[30rem] w-full rounded-t-lg object-cover object-center group-hover:opacity-25"
+            className="h-[20rem] w-full rounded-t-lg object-cover object-center group-hover:opacity-25"
           />
+          <div className='absolute top-5 left-5 opacity-0 group-hover:opacity-100'>
+            <p>タイトル:</p>
+            <p>　{title}</p>
+            <p>著者: {author}</p>
+          </div>
         </div>
       </a>
       <div className='flex rounded-lg'>
-        <a href="#" className="py-1 w-full text-center border-r">借りる</a>
+        <a href="#" className={`py-1 w-full text-center border-r ${loanable ? '' : 'text-gray-400 cursor-not-allowed'}`}>借りる</a>
         <a href="#" className="py-1 w-full text-center">借りたい</a>
       </div>
     </div>
@@ -34,35 +39,37 @@ function BookList(): ReactElement {
     {
       imageUrl:
         'https://www.oreilly.co.jp/books/images/picture_large978-4-87311-565-8.jpeg',
-      altText: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-      title: 'リーダブルコード'
+      title: 'リーダブルコード',
+      author: 'Dustin Boswell',
+      loanable: false
     },
     {
       imageUrl:
         'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-      altText: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-      title: 'Nomad Tumbler'
+      title: 'Nomad Tumbler',
+      author: 'Dustin Boswell',
+      loanable: true
     },
     {
       imageUrl:
         'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-      altText: 'Person using a pen to cross a task off a productivity paper card.',
-      title: 'Focus Paper Refill'
+      title: 'Focus Paper Refill',
+      author: 'Dustin Boswell',
+      loanable: true
     },
     {
       imageUrl:
         'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-      altText: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-      title: 'Machined Mechanical Pencil'
+      title: 'Machined Mechanical Pencil',
+      author: 'Dustin Boswell',
+      loanable: true
     }
   ];
 
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
-
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
           {books.map((book, index) => (
             <BookCard key={index} {...book} />
           ))}
